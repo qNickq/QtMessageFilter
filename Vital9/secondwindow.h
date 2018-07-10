4a368509a2cbcb71acba637f9ee897bcc2a24a40
Vitaly Ruzanov <vp.ruzanov@gmail.com>
2018-07-10 14:20:00 +0300

    Добавил таблицу

---
+++ Vital9/secondwindow.h
@@ -0,0 +1,33 @@
+#ifndef SECONDWINDOW_H
+#define SECONDWINDOW_H
+
+#include <QDialog>
+#include <QTableWidget>
+#include <set>
+
+namespace Ui {
+class SecondWindow;
+}
+
+class SecondWindow : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit SecondWindow(QWidget *parent = 0);
+    ~SecondWindow();
+
+private slots:
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_clicked();
+
+private:
+    Ui::SecondWindow *ui;
+    QString string;
+    enum class MyTypes;
+    std::set<MyTypes>FullSet;
+    std::set<MyTypes>ChangedSet;
+};
+
+#endif // SECONDWINDOW_H
